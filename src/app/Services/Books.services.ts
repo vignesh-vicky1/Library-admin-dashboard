4742e16bbc2270c00f372991750f810d3808114a
vignesh-vicky1 <vigneshvickybsc1999@gmail.com>
2024-02-08 12:15:00 +0530

    Create Delete Function

--- src/app/Services/Books.services.ts
+++ src/app/Services/Books.services.ts
@@ -1,26 +1,15 @@
-import {EventEmitter, Injectable, OnInit} from "@angular/core";
+import { EventEmitter, Injectable, OnInit } from '@angular/core';
 
-import {HttpClient} from "@angular/common/http";
-import {issuebooktype} from "./IssuedBooktype";
-import {Dataservice} from "./Data.services";
-import {map} from "rxjs";
+import { HttpClient } from '@angular/common/http';
+import { issuebooktype } from './IssuedBooktype';
+import { Dataservice } from './Data.services';
 
+@Injectable({ providedIn: 'root' })
+export class BookData {
+  constructor(private Http: HttpClient) {}
 
-@Injectable({providedIn:"root"})
-export class BookData  {
-  constructor(private Http:HttpClient) {
-
-
-  }
-
-
-  BookCount:number=0;
-  StudentCount:number=0;
-  IssuedBookDetails:issuebooktype[]=[]
- book:number=0
-  
-
-
-
-
+  BookCount: number = 0;
+  StudentCount: number = 0;
+  IssuedBookDetails: issuebooktype[] = [];
+  book: number = 0;
 }
