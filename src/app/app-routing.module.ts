ba3bbf5e265783aa3c89348541b6b8a87a7febe9
vignesh-vicky1 <vigneshvickybsc1999@gmail.com>
2024-02-09 17:55:22 +0530

    Created Individual ROUTING module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { Books } from './Managebooks/books.component';
+import { Home } from './Home/home.component';
+import { StudentComponent } from './ManageStudent/student.component';
+import { IssuebookComponent } from './IssueBook/issuebook.component';
+import { ReturnBook } from './ReturnBook/returnbook.component';
+import { AppComponent } from './app.component';
+import { Records } from './Records/records.component';
 
-const routes: Routes = [];
+const routes=[
+  {path:"Books",component:Books},
+  {path:'Home', component: Home},
+  {path: 'Students',component: StudentComponent},
+  {path: "IssueBooks",component: IssuebookComponent},
+  {path: "ReturnBook",component: ReturnBook},
+  {path:'LogIn',component: AppComponent},
+  {path:'Records',component: Records}
+]
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
