9ab1a861525ba8ca14c2608da89a4df775277b0a
vignesh-vicky1 <vigneshvickybsc1999@gmail.com>
2024-02-16 00:31:09 +0530

    Implement: New searching feature

--- src/app/Records/records.component.ts
+++ src/app/Records/records.component.ts
@@ -1,9 +1,47 @@
+import { HttpClient } from "@angular/common/http";
 import { Component } from "@angular/core";
+import { map } from "rxjs";
+import { Dataservice } from "../Services/Data.services";
 
 @Component({selector:'Records-app',
 templateUrl:'cords.component.html',
 styleUrls:['records.component.css']})
 export class Records{
-  constructor() {
+  constructor(private Http:HttpClient) {
+
   }
+  FinalData:Dataservice[]=[]
+  error:boolean=false
+
+search(event:HTMLInputElement){
+    console.log(event.value)
+  this.Http.get<any>("https://personandb-default-rtdb.firebaseio.com/Books.json")
+    .pipe(map((res)=>{
+      let Data:Dataservice[]=[]
+      for(let a in res){
+         Data.push(res[a])
+
+      }
+      return Data
+    })).subscribe((res1)=>{
+
+    console.log(res1)
+    //FILTER the VAlue which is equal to user input//
+    this.FinalData=res1.filter((res2)=>{
+     return res2.BookName.toLowerCase() == event.value
+    })
+    if(this.FinalData.length>=1){
+        this.error=false
+    }else{
+      this.error=true
+    }
+    console.log(this.error)
+    console.log(this.FinalData)
+  })
+
+
+}
+
+
+
 }
